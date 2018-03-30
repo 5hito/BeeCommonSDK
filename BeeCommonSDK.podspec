51307590e55ea7186e5775fd4c53c81112befbcf
5hito <beemans@foxmail.com>
2018-03-30 14:36:29 +0800

    1.0.5

--- BeeCommonSDK.podspec
+++ BeeCommonSDK.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "BeeCommonSDK"
-  s.version      = "1.0.4"
+  s.version      = "1.0.5"
   s.summary      = "集合基本使用功能"
   s.description  = <<-DESC
     0.1.x:基础功能合集使用
---
+++ lib/Header/BeeNotificationMacroHeader.h
@@ -0,0 +1,14 @@
+//
+//  BeeNotificationMacroHeader.h
+//  BeeCommonExample
+//
+//  Created by linshucan on 2018/3/30.
+//  Copyright © 2018年 Bee. All rights reserved.
+//
+
+#ifndef BeeNotificationMacroHeader_h
+#define BeeNotificationMacroHeader_h
+
+#define BeeAppNetworkingChangeNotification         @"BeeAppNetworkingChangeNotification"    //网络变化通知
+
+#endif /* BeeNotificationMacroHeader_h */
