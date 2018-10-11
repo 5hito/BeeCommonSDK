df4e180eb074d85091a7d9ccbbb74c493b223dd9
5hito <beemans@foxmail.com>
2018-10-11 15:31:55 +0800

    1.3.1

--- lib/Header/BeeMacro.h
+++ lib/Header/BeeMacro.h
@@ -9,11 +9,11 @@
 #ifndef BeeMacro_h
 #define BeeMacro_h
 
-#define BeeSTAUS_HEIGHT (BeeIsIphoneX? 44 : 20)
+#define BeeSTAUS_HEIGHT (BeeIsUpIphoneX? 44 : 20)
 #define BeeNAVIGATION_TOP_HEIGHT (44)//横屏32
 #define BeeTOPBAR_HEGITH (BeeSTAUS_HEIGHT+BeeNAVIGATION_TOP_HEIGHT)
 #define BeeToolBar_Height       49//(47)
-#define BeeSafeAreaBottomHeight (BeeIsIphoneX? 34 : 0)
+#define BeeSafeAreaBottomHeight (BeeIsUpIphoneX? 34 : 0)
 #define BeeBottomToolBarHeight (BeeToolBar_Height+BeeSafeAreaBottomHeight)
 
 #define BeeScreenBounds ([UIScreen mainScreen].bounds)
@@ -27,19 +27,26 @@
 #define BeeIsIphone6  (CGRectGetHeight([UIScreen mainScreen].bounds) == 667 ? YES : NO)
 #define BeeIsIphone6P (CGRectGetHeight([UIScreen mainScreen].bounds) == 736 ? YES : NO)
 #define BeeIsIphoneX  (CGRectGetHeight([UIScreen mainScreen].bounds) == 812 ? YES : NO)
+#define BeeIsIphoneXMax  (CGRectGetHeight([UIScreen mainScreen].bounds) == 896 ? YES : NO)
 
 #define BeeIsUpIphone5 (CGRectGetHeight([UIScreen mainScreen].bounds) > 568 ? YES : NO)
 #define BeeIsUpIphone6 (CGRectGetHeight([UIScreen mainScreen].bounds) > 667 ? YES : NO)
-#define BeeIphone4Width    (320) //640
-#define BeeIphone4Height   (480) //960
-#define BeeIphone5Width    (320) //640
-#define BeeIphone5Height   (568) //1136
-#define BeeIphone6Width    (375) //750
-#define BeeIphone6Height   (667) //1334
-#define BeeIphone6PWidth   (414) //1242
-#define BeeIphone6PHeight  (736) //2208
-#define BeeIphoneXWidth    (375) //1125
-#define BeeIphoneXHeight   (812) //2236
+#define BeeIsUpIphoneX (CGRectGetHeight([UIScreen mainScreen].bounds) >= 812 ? YES : NO)
+
+#define BeeIphone4Width         (320) //640
+#define BeeIphone4Height        (480) //960
+#define BeeIphone5Width         (320) //640
+#define BeeIphone5Height        (568) //1136
+#define BeeIphone6Width         (375) //750
+#define BeeIphone6Height        (667) //1334
+#define BeeIphone6PWidth        (414) //1242
+#define BeeIphone6PHeight       (736) //2208
+#define BeeIphoneXWidth         (375) //1125
+#define BeeIphoneXHeight        (812) //2436
+#define BeeIphoneXRWidth        (414) //828
+#define BeeIphoneXRHeight       (896) //1792
+#define BeeIphoneXSMaxWidth     (414) //1242
+#define BeeIphoneXSMaxHeight    (896) //2688
 
 #define BeeGrayColor ([UIColor colorWithRed:215/255.0f green:215/255.0f blue:215/255.0f alpha:0.5])
 #define BeeRandomColor ( CGFloat red = ( arc4random() % 256 / 255.0 ); CGFloat green = ( arc4random() % 256 / 255.0 ); CGFloat blue = ( arc4random() % 256 / 255.0 ); return [UIColor colorWithRed:red green:green blue:blue alpha:1.0f]; )
