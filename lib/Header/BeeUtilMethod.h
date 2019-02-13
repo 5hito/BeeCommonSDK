7ec46c7d4bd9851bfaea3c65925a9c638644827f
5hito <beemans@foxmail.com>
2019-02-13 11:15:25 +0800

    1.5.7

---
+++ lib/Header/BeeUtilMethod.h
@@ -0,0 +1,17 @@
+//
+//  BeeUtilMethod.h
+//  BeeCommonSDK
+//
+//  Created by linshucan on 2019/2/13.
+//  Copyright © 2019年 Bee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BeeUtilMethod : NSObject
+
++ (CGFloat)calculationWidth:(CGFloat)width;
++ (CGFloat)calculationHeight:(CGFloat)height;
+
+@end
+
